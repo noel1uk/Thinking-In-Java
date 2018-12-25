bf41dd33ebbab8b879d28fd656daa7aa1606bc19
Noel Vock <noelvock@noels-mbp.home>
2018-12-25 08:45:15 +0000

    example of class with uninitialized variables that prints the default values

---
+++ EverythingIsAnObject/DataOnly.java
@@ -0,0 +1,13 @@
+public class DataOnly {
+
+    int i;
+    double d;
+    boolean b;
+
+    public static void main(String[] args) {
+        DataOnly data = new DataOnly();
+        System.out.println(data.i);
+        System.out.println(data.d);
+        System.out.println(data.b);
+    }
+}
