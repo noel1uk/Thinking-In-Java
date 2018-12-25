838f69f440c91b28185b2b532a2c156526dd1dbb
Noel Vock <noelvock@noels-mbp.home>
2018-12-25 14:09:07 +0000

    example demonstrating autoboxing works for all the primitive types

---
+++ EverythingIsAnObject/AutoBox.java
@@ -0,0 +1,37 @@
+class AutoBox {
+
+    public static void main(String[] args) {
+        boolean b = false;
+        char c = 'c';
+        byte by = 1;
+        short s = 10;
+        int i = 0;
+        long l = 100;
+        float f = 0.10f;
+        double d = 0.11;
+        Boolean B = b;
+        System.out.println(b);
+        System.out.println(B);
+        Character C = c;
+        System.out.println(c);
+        System.out.println(C);
+        Byte By = by;
+        System.out.println(by);
+        System.out.println(By);
+        Short S = s;
+        System.out.println(s);
+        System.out.println(S);
+        Integer I = i;
+        System.out.println(i);
+        System.out.println(I);
+        Long L = l;
+        System.out.println(l);
+        System.out.println(L);
+        Float F = f;
+        System.out.println(f);
+        System.out.println(F);
+        Double D = d;
+        System.out.println(d);
+        System.out.println(D);
+    }
+}
