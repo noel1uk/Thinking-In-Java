310439628e12f95977a4abbf5ed4b71e3dbe031b
Noel Vock <noelvock@noels-mbp.home>
2018-12-25 14:13:37 +0000

    example of a program that takes command line arguments and prints to console

---
+++ EverythingIsAnObject/CommandLineArguments.java
@@ -0,0 +1,9 @@
+class CommandLineArguments {
+
+    public static void main(String[] args) {
+        System.out.println("Argument 0: " + args[0]);
+        System.out.println("Argument 1: " + args[1]);
+        System.out.println("Argument 2: " + args[2]);
+
+    }
+}
