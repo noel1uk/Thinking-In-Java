b395414d5e18308ba54425212ce9c9a464beab23
Noel Vock <noel1uk@gmail.com>
2018-12-31 15:24:20 +0000

    test

--- Operators/DogTest.java
+++ Operators/DogTest.java
@@ -31,10 +31,19 @@ class DogTest {
         dog2.setName("Scruffy");
         dog2.setBark("Wurf");
 
+        Dog dog3 = dog1;
+
         System.out.println("Dog1 is: " + dog1.showName());
         System.out.println("Dog1 says: " + dog1.showBark());
 
         System.out.println("Dog2 is: " + dog2.showName());
         System.out.println("Dog2 says: " + dog2.showBark());
+
+        if (dog1 == dog3) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        System.out.println(dog1.equals(dog3));
     }
 }
