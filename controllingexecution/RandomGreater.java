1457154c20e984727192cf98e827ca9191f6cc99
Noel Vock <noel1uk@gmail.com>
2019-01-03 21:15:03 +0000

    program that generates 25 random int values. for each value use an if-else statement to clarify equality

---
+++ controllingexecution/RandomGreater.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class RandomGreater {
+    public static void main(String[] args) {
+        int round = 0;
+        while(round <= 25) {
+            Random rand = new Random();
+            int number1 = rand.nextInt();
+            int number2 = rand.nextInt();
+            System.out.println("round " + round + ": ");
+            if (number1 > number2) {
+                System.out.println(number1 + " is greater then " + number2);
+            } else if (number1 == number2) {
+                System.out.println(number1 + " is equal to " + number2);
+            } else {
+                System.out.println(number1 + " is less then " + number2);
+            }
+            round++;
+        }
+    }
+}
