6e5be60a33eb055ee1b4b488646532900b131ff0
Noel Vock <noel1uk@gmail.com>
2019-01-05 09:15:21 +0000

    example switch statement in a for loop

---
+++ controllingexecution/SwitchStatement.java
@@ -0,0 +1,23 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 6; i++) {
+            switch(i) {
+                case 1 :
+                    System.out.println(i);
+                    break;
+                case 2 :
+                    System.out.println(i);
+                    break;
+                case 3 :
+                    System.out.println(i);
+                    break;
+                case 4 :
+                    System.out.println(i);
+                    break;
+                default:
+                    System.out.println("complete");
+            }
+        }
+    }
+}
