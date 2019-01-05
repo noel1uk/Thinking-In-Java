2cb2015606b9e062bb07f3bc1a1452af867c5f68
Noel Vock <noel1uk@gmail.com>
2019-01-05 15:17:06 +0000

    example of a default or no-args constructor

---
+++ initializationandcleanup/DefaultConstructor.java
@@ -0,0 +1,8 @@
+public class DefaultConstructor {
+    DefaultConstructor() {
+        System.out.println("inside the no args constructor");
+    }
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+}
