b9b32a48162771eab8c1742b33eee210cccf9a4b
Noel Vock <noel1uk@gmail.com>
2019-01-07 06:48:33 +0000

    array of String objects with a string for each element, printed from a for loop

---
+++ initializationandcleanup/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        String [] stringArray = {"foo", "bar", "muu"};
+        for(int i = 0; i < stringArray.length; i++) {
+            System.out.println(stringArray[i]);
+        }
+    }
+}
